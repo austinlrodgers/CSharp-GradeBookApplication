7b3d0aa61378c55c8471c9f784e49432f61a49ad
Austin Rodgers <austinlrod@gmail.com>
2019-02-25 09:28:59 -0500

    Completed the Module

---
+++ GradeBook/GradeBooks/RankedGradebook.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradebook : BaseGradeBook
+    {
+        public RankedGradebook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
