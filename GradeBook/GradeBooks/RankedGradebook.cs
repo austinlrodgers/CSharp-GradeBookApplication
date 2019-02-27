0cd9573f0d436d0b6915295e627371d50898375b
Austin Rodgers <austinlrod@gmail.com>
2019-02-27 09:40:12 -0500

    No idea why this isnt working

--- GradeBook/GradeBooks/RankedGradebook.cs
+++ GradeBook/GradeBooks/RankedGradebook.cs
@@ -9,5 +9,32 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (base.Students.Count < 5) throw new InvalidOperationException("Ranked Grading needs at least 5 students");
+
+            char letterGrade = 'F';
+            var threshold = (int)Math.Ceiling(base.Students.Count * 0.2);
+            var grades = base.Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+            {
+                letterGrade = 'A';
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                letterGrade = 'B';
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                letterGrade = 'C';
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            {
+                letterGrade = 'D';
+            }
+
+            return letterGrade;
+        }
     }
 }
